48b923a8cc326c6bbb37bc9ab0e758f1899855a2
Chelsea Van Coller <61380491+freckles98@users.noreply.github.com>
2025-11-26 11:43:45 +0300

    Some small updates

--- js/main.js
+++ js/main.js
@@ -128,28 +128,6 @@
         time: 2000
     });
 
-
-    // Testimonials carousel
-    $(".testimonials-carousel").owlCarousel({
-        center: true,
-        autoplay: true,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-    });
     
     
     // Related post carousel
